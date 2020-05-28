ea821ce1cc9321a0e752e1e7bdf15cc10cca2e79
github username <github email address>
2020-05-28 00:04:31 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+  # Oops. Error. Gotta tell those routes explicitly which controller actions will handle them. Okay, let's make it look more like this:
+
+  # get 'authors/:id/posts', to: 'authors#posts_index'
+  # get 'authors/:id/posts/:post_id', to: 'authors#post' same as lines 4
 
 end
